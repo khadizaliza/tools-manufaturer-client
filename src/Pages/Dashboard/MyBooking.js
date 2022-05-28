a351250a0d81888065af0f90a1bb2bec59cb90cc
khadizaliza <khadizaakter19836@gmail.com>
2022-05-28 20:02:54 +0600

    create-table

--- src/Pages/Dashboard/MyBooking.js
+++ src/Pages/Dashboard/MyBooking.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyBooking = () => {
+    const [booked, setBooked] = useState([]);
+    const [user] = useAuthState(auth)
+
+    useEffect( () =>{
+        if(user) {
+            fetch(`http://localhost:5000/service?customar=${user.email}`)
+        .then(res=>res.json())
+        .then(data => setBooked(data));
+        }
+    }, [user])
     return (
         <div>
-            <h2>booking</h2>
+            <h2>booking: {booked.length}</h2>
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Minimum Order Quentity</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+          booked.map((a, index) =><tr>
+            <th>{index + 1}</th>
+            <td>{a.name}</td>
+            <td>$ {a.price}</td>
+            <td>{a.minimumOrderQuentity}</td>
+          </tr>)
+      }
+      
+      
+      
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
